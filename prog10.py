fc34d61a3ec245750a408e885f00b42303a4ce9d
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-10-01 21:45:13 +0530

    Create prog10.py

---
+++ prog10.py
@@ -0,0 +1,9 @@
+mydict = {
+"maa":"mother", 
+"papa":"father",
+"dost":"friend",
+"parivaar":"family",
+}
+
+a= input("Enter the Hindi word\n")
+print("The english word for the Hindi word is " , mydict[a])

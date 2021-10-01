007c598b94a9a5f97bc56036637fdb01ee72b92e
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-10-01 21:50:47 +0530

    Create prog15.py

---
+++ prog15.py
@@ -0,0 +1,8 @@
+num1= int(input("enter no.1:"))
+num2= int(input("enter no.2:"))
+num3= int(input("enter no.3:"))
+num4= int(input("enter no.4:"))
+
+list1 =[num1,num2,num3,num4]
+list1.sort()
+print("the greatest of 4no. is: ", list1[3])

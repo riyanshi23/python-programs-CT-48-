66e58e53a3d0a6ff16c3d2093eeb1c01d502e7e1
riyanshi23 <91719142+riyanshi23@users.noreply.github.com>
2021-10-09 21:41:51 +0530

    Create prog22.py

---
+++ prog22.py
@@ -0,0 +1,16 @@
+# using for loop
+
+num=input("Enter the number for multiplication table: ")
+
+print("Using for loop")
+for i in range(1,11):
+    print(num,'x ',i,' = ',num*i) 
+
+i = 1
+print("Using for loop")
+
+#using while loop
+
+while( i <= 10):
+    print(num,'x ',i,' = ',num*i)
+    i+=1
